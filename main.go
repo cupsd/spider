b7134b3890de8c614a8adc1efe772834223e1c48
leo <template.xu@gmail.com>
2019-12-10 15:42:14 +0800

    coly test

--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"path"
 	"strconv"
@@ -14,17 +12,14 @@ import (
 )
 
 type Movie struct {
-	Title       string
-	Url         string
-	DownloadAdd []string
+	Title string
+	Url   string
 }
 
 func main() {
-	var d *colly.Collector
+
 	movies := []*Movie{}
 	c := colly.NewCollector()
-
-	d = c.Clone()
 
 	q, _ := queue.New(15, &queue.InMemoryQueueStorage{MaxSize: 10000})
 
@@ -55,20 +50,8 @@ func main() {
 	q.Run(c)
 
 	for _, movie := range movies {
-		getDownloadAddr(d, movie)
+		fmt.Printf("%#v\n", movie)
 	}
-
-	// file,err := os.OpenFile("movie.txt",os.O_CREATE|os.O_WRONLY|os.O_APPEND,0644)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	data, err := json.Marshal(movies)
-
-	err = ioutil.WriteFile("movie.txt", data, 0644)
-	if err != nil {
-		log.Fatal(err)
-	}
-
 }
 
 // 获取总页数
@@ -86,14 +69,4 @@ func getPageCount() (count int, err error) {
 	c.Visit("http://gaoqing.la/1080p")
 
 	return
-}
-
-func getDownloadAddr(d *colly.Collector, movie *Movie) {
-
-	d.OnHTML("div#post_content p", func(e *colly.HTMLElement) {
-		// fmt.Println(e.ChildAttrs("span a", "href"))
-		movie.DownloadAdd = append(movie.DownloadAdd, e.ChildAttrs("span a", "href")...)
-	})
-
-	d.Visit(movie.Url)
 }
